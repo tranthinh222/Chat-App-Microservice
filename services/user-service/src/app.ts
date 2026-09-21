8acf80e3d5fa03fa4e5efb8942a3eebb0d00ea7a
Trần Quốc Thịnh <tranquocthinh2221980@gmail.com>
2026-09-21 18:09:34 +0700

    feat: implement Prisma client for database connection and update health check endpoints

--- services/user-service/src/app.ts
+++ services/user-service/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { prisma } from './config/prisma.js'
 
 const app = express()
 app.use(express.json())
@@ -10,10 +11,21 @@ app.get('/health/live', (req, res) => {
   })
 })
 
-// app.get('health/ready', async (req, res) => {
-//   const databaseReady = await checkDatabaseConnection()
-//   res.status(databaseReady ? 200 : 503).json({
-//     status: databaseReady ? 'ready' : 'not_ready',
-//   })
-// })
+app.get('/health/ready', async (req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`
+
+    res.status(200).json({
+      service: 'user-service',
+      status: 'READY',
+      database: 'UP',
+    })
+  } catch {
+    res.status(503).json({
+      service: 'user-service',
+      status: 'READY',
+      database: 'DOWN',
+    })
+  }
+})
 export default app
